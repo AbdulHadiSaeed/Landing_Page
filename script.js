ec2d0df19c90aa66b6929718350b5e610f0aaa12
Abdul Hadi Saeed <abdulhadi.50@hotmail.com>
2025-02-27 17:53:26 -0500

    script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+// Set a timer to wait for a few seconds (e.g., 3 seconds)
+setTimeout(function() {
+    // After the wait, redirect to your portfolio website
+    window.location.href = "https://aludoors.com.au/"; // Replace with your actual portfolio link
+}, 3000); // 3000 milliseconds = 3 seconds
